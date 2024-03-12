b74b79ce8217afc6672eecdea85e9ec4da34acb0
gleg0r <egor103230@gmail.com>
2024-03-13 00:34:59 +0300

    init eslint, prettier, store

---
+++ .eslintrc.js
@@ -0,0 +1,59 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'prettier'
+  ],
+  'overrides': [
+    {
+      'env': {
+        'node': true
+      },
+      'files': [
+        '.eslintrc.{js,cjs}'
+      ],
+      'parserOptions': {
+        'sourceType': 'script'
+      }
+    }
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react'
+  ],
+  'rules': {
+    'indent': [
+      'warn',
+      2
+    ],
+    'quotes': [
+      'warn',
+      'single'
+    ],
+    'semi': [
+      'warn',
+      'always'
+    ],
+    'max-len': [
+      'warn',
+      {'code': 80}
+    ],
+    'import/no-unresolved': [
+      0, 
+      { 'caseSensitive': false }
+    ],
+    'no-magic-numbers': 'warn',
+		'react-hooks/exhaustive-deps': 0,
+		'no-unused-vars': 'warn',
+  }
+};
