89ef1f09e1f9c5a3ae2838b4352f1a490956af8b
53400755Maria <141526746+53400755Maria@users.noreply.github.com>
2024-03-14 21:16:29 +0300

    new

--- src/components/ui/CheckBox/CheckBox.jsx
+++ src/components/ui/CheckBox/CheckBox.jsx
@@ -1,10 +1,11 @@
+
 import s from './style.module.scss';
 
 export default function CheckBox() {
-  return (
-    <div className={s.checkbox}>
-      {/* Убираешь этот комментарий 
-        и фигурные скобки после чего здесь можешь писать разметку */}
-    </div>
-  );
+return (
+<div className={s.checkbox}>
+  <input type="checkbox" id="privacy-policy" name="privacy-policy" required />
+  <label>I agree with <span>Teams of service</span> and <span>Privacy policy</span></label>
+</div>
+);
 }
