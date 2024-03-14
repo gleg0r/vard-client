ed1a38fa07aa84429d1ea58a766ae6a0b4f6e59e
gleg0r <egor103230@gmail.com>
2024-03-14 23:20:14 +0300

    edit: structure of sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Input from './components/ui/Input/Input';
+import CheckBox from './components/ui/CheckBox/CheckBox';
 
 function App() {
   return (
     <div className="App">
       <Input placeholder={'Email'} />
+      <CheckBox />
     </div>
   );
 }
--- src/components/ui/CheckBox/CheckBox.jsx
+++ src/components/ui/CheckBox/CheckBox.jsx
@@ -1,11 +1,18 @@
-
 import s from './style.module.scss';
 
 export default function CheckBox() {
-return (
-<div className={s.checkbox}>
-  <input type="checkbox" id="privacy-policy" name="privacy-policy" required />
-  <label>I agree with <span>Teams of service</span> and <span>Privacy policy</span></label>
-</div>
-);
+  return (
+    <div className={s.checkbox}>
+      <input
+        type="checkbox"
+        id="privacy-policy"
+        name="privacy-policy"
+        required
+      />
+      <label>
+        I agree with <span>Teams of service</span> and{' '}
+        <span>Privacy policy</span>
+      </label>
+    </div>
+  );
 }
