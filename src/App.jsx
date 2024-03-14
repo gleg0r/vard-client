f3fb7d7d358bc62458c5fceb1d2e36a88c9accd8
gleg0r <egor103230@gmail.com>
2024-03-14 19:20:40 +0300

    create routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Input from './components/ui/Input/Input';
 
 function App() {
   return (
     <div className="App">
       <Input placeholder={'Email'} />
+      <Routes>
+        <Route>
+          <Route />
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/core/constants/Routes.js
@@ -0,0 +1,12 @@
+export const RoutesName = {
+  Home: '/',
+  Dashboards: '/dashboards',
+  Connections: '/connections',
+  Files: '/files',
+  Charts: '/charts',
+  Wiki: '/wiki',
+  BestPractices: '/best-practices',
+  Community: '/community',
+  LogIn: '/login',
+  Register: '/register',
+};
