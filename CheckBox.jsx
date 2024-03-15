744583cfa223d6e9fcbe59e6fa8eea46b5fdf85d
53400755Maria <141526746+53400755Maria@users.noreply.github.com>
2024-03-15 23:50:05 +0300

    Add files via upload

--- CheckBox.jsx
+++ CheckBox.jsx
@@ -1,24 +1,25 @@
-import React, { useState } from 'react'; 
-import s from './style.module.scss'; 
- 
-export default function CheckBox() { 
-  const [isActive, setIsActive] = useState(false); 
- 
-  const handleCheckBoxClick = () => { 
-    setIsActive(!isActive); 
-  }; 
- 
-  return ( 
-    <div className={`${s.checkbox} ${s.checkboxContainer}`}> 
-      { } 
-      <div 
-        className={`${s.checkboxSquare} ${isActive ? s.active : ''}`} 
-        onClick={handleCheckBoxClick} 
-      /> 
-      <label className={s.checkboxLabel}> 
-        I agree with <span className={s.checkboxSpan}>Teams of service</span> and{' '} 
-        <span className={s.checkboxSpan}>Privacy policy</span> 
-      </label> 
-    </div> 
-  ); 
-} 
+import React, { useState } from 'react';
+import s from './style.module.scss';
+import cn from 'classnames';
+
+export default function CheckBox() {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleCheckBoxClick = () => {
+    setIsActive(!isActive);
+  };
+
+  return (
+    <div className={cn('checkbox__Container', { [s.checkbox__Container]: true })}>
+      <div
+        className={cn('element__modifier', { [s.element__modifier]: true, [s.active]: isActive })}
+        onClick={handleCheckBoxClick}
+      />
+     <label className={cn('block__element', { [s.block__element]: true })}>
+  I agree with{' '}
+  <span className={cn('checkbox__element', { [s.checkbox__element]: true })}> Teams of service </span> and {' '}
+  <span className={cn('checkbox__element', { [s.checkbox__element]: true })}> Privacy policy</span>
+</label>
+    </div>
+  );
+}
