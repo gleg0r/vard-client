3251aa4d10428928fc53130ce4ca24d86b0677e4
gleg0r <egor103230@gmail.com>
2024-03-15 23:32:34 +0300

    add login page

--- src/components/ui/Button/Button.jsx
+++ src/components/ui/Button/Button.jsx
@@ -1,6 +1,10 @@
 import s from './style.module.scss';
 import cn from 'classnames';
 
-export default function Button({ className, children }) {
-  return <button className={cn(s.button, className)}>{children}</button>;
+export default function Button({ className, children, onClick }) {
+  return (
+    <button onClick={onClick} className={cn(s.button, className)}>
+      {children}
+    </button>
+  );
 }
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable indent */
+import { useState } from 'react';
+import cn from 'classnames';
 import s from './style.module.scss';
 import Input from '../../components/ui/Input/Input';
 import CheckBox from '../../components/ui/CheckBox/CheckBox';
@@ -7,38 +10,63 @@ import google from '../../assets/images/google_auth.svg';
 import github from '../../assets/images/github_auth.svg';
 
 export default function AuthPage() {
+  const [isLoginPage, setIsLoginPage] = useState(false);
+
+  const changeToLogin = () => setIsLoginPage(true);
+
   return (
     <div className={s.auth}>
       <div className={s.auth__header}>
-        <h3 className={s.auth__title}>Welcome to VARD</h3>
+        <h3 className={s.auth__title}>
+          {isLoginPage ? 'Log in' : 'Welcome'} to VARD
+        </h3>
         <img src={logo} alt="logo" />
       </div>
       <div className={s.auth__main}>
         <div className={s.auth__form}>
-          <Input placeholder={'Name'} type={'name'} />
+          {isLoginPage ? '' : <Input placeholder={'Name'} type={'name'} />}
           <Input placeholder={'Email'} type={'email'} />
           <Input placeholder={'Password'} type={'password'} />
-          <Input placeholder={'Confirm password'} type={'password'} />
+          {isLoginPage ? (
+            ''
+          ) : (
+            <Input placeholder={'Confirm password'} type={'password'} />
+          )}
         </div>
-        <CheckBox />
+        {isLoginPage ? '' : <CheckBox />}
       </div>
       <div className={s.auth__footer}>
         <div className={s.auth__btns}>
-          <Button className={s.auth__create}>Create account</Button>
-          <p className={s.auth__text}>or</p>
-          <Button className={s.auth__login}>Log in</Button>
+          {isLoginPage ? (
+            ''
+          ) : (
+            <Button className={s.auth__create}>Create account</Button>
+          )}
+
+          {isLoginPage ? '' : <p className={s.auth__text}>or</p>}
+
+          <Button
+            onClick={() => changeToLogin()}
+            className={cn(isLoginPage ? s.auth__login_active : s.auth__login)}
+          >
+            Log in
+          </Button>
         </div>
-        <div className={s.auth__socials}>
-          <p className={s.auth__signup}>or Sign Up with</p>
-          <div className={s.auth__socials_btns}>
-            <Button className={s.auth__social}>
-              <img src={google} alt="google auth" />
-            </Button>
-            <Button className={s.auth__social}>
-              <img src={github} alt="google auth" />
-            </Button>
+        {isLoginPage ? (
+          ''
+        ) : (
+          <div className={s.auth__socials}>
+            <p className={s.auth__signup}>or Sign Up with</p>
+            <div className={s.auth__socials_btns}>
+              <Button className={s.auth__social}>
+                <img src={google} alt="google auth" />
+              </Button>
+              <Button className={s.auth__social}>
+                <img src={github} alt="google auth" />
+              </Button>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
