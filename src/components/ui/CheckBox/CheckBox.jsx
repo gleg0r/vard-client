469bf0737c19c507efe2316dea804e91712f27d0
gleg0r <egor103230@gmail.com>
2024-03-16 15:00:11 +0300

    edit: checkbox

--- src/components/ui/CheckBox/CheckBox.jsx
+++ src/components/ui/CheckBox/CheckBox.jsx
@@ -1,27 +1,26 @@
-
-import React, { useState } from 'react';
+import { useState } from 'react';
 import s from './style.module.scss';
 import cn from 'classnames';
 
 export default function CheckBox() {
   const [isActive, setIsActive] = useState(false);
 
-  const handleCheckBoxClick = () => { 
-    setIsActive(prev => !prev); 
+  const handleCheckBoxClick = () => {
+    setIsActive((prev) => !prev);
   };
-  
 
   return (
-    <div className={cn('checkbox__Container', { [s.checkbox__Container]: true })}>
+    <div className={cn(s.checkbox)}>
       <div
-        className={cn('checkbox__Container__modifier', { [s.checkbox__Container__modifier]: true, [s.active]: isActive })}
+        className={cn(s.checkbox__item, isActive ? s.active : '')}
         onClick={handleCheckBoxClick}
       />
-     <label className={s.checkbox__Container__block}>
-  I agree with{' '}
-  <span className={s.checkbox__Container__element}> Teams of service </span> and {' '}
-  <span className={s.checkbox__Container__element}> Privacy policy</span>
-</label>
+      <label onClick={handleCheckBoxClick} className={s.checkbox__text}>
+        I agree with
+        <span className={s.checkbox__accent}>Teams of service</span>
+        and
+        <span className={s.checkbox__accent}>Privacy policy</span>
+      </label>
     </div>
   );
 }
