a4c6b35d2d43aff4175e57ae3912b7ac06d973c5
gleg0r <egor103230@gmail.com>
2024-03-17 13:56:48 +0300

    add: header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 import s from './style.module.scss';
 import logo from '../../assets/images/logo.svg';
 
-export default function Header {
+export default function Header() {
   return (
     <header className={s.header}>
       <div className={s.header__logo}>
-        <img src={logo} alt='logo'/>
+        <img src={logo} alt="logo" />
       </div>
     </header>
-  )
+  );
 }
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,12 @@
 import s from './style.module.scss';
 import { Outlet } from 'react-router-dom';
+import Header from '../components/Header/Header';
 
 export default function LayoutMain() {
   return (
     <div className={s.wrapper}>
       <div className={s.wrapper__container}>
-        <h1>Layout</h1>
+        <Header />
         <Outlet />
       </div>
     </div>
