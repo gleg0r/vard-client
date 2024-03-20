e782e450662f0f3ff86bbdbc04a097f757f3add0
gleg0r <egor103230@gmail.com>
2024-03-20 15:57:03 +0300

    add icons, add onChange for input

--- src/components/ui/Input/Input.jsx
+++ src/components/ui/Input/Input.jsx
@@ -1,9 +1,10 @@
 import s from './style.module.scss';
 import cn from 'classnames';
 
-export default function Input({ placeholder, className, type }) {
+export default function Input({ placeholder, className, type, onChange }) {
   return (
     <input
+      onChange={onChange}
       className={cn(s.input, className)}
       placeholder={placeholder}
       type={type}
